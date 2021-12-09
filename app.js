31c4c335d6ad46a1caba61e92429279ef3e3b157
NWOS <somkumarav@gmail.com>
2021-12-09 22:26:52 +0530

    made some changes

--- app.js
+++ app.js
@@ -2,60 +2,121 @@ const container = document.querySelector('.container');
 
 const ar = [
   {
-    name: 'Somkumar V A',
-    location: 'Guruvayoor',
+    name: 'Vishnu Anilkumar (C)',
+    dob: '27 January 2001',
+    location: 'Puzhakkal',
     achivements: 'Web Developer',
-    instagram: 'https://instagram.com',
-    photo: './assets/Somkumar.jpeg',
-  },
-  {
-    name: 'Sherin Akbar',
-    location: 'Guruvayoor',
-    achivements: 'Web Developer',
-    instagram: 'https://www.instagram.com/__sherin__akbar__/',
-    photo:
-      'https://instagram.fcok6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/231774616_503739087551493_8146171040485037386_n.jpg?_nc_ht=instagram.fcok6-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=nqSvjpr3unIAX9jBDLY&edm=AP_V10EBAAAA&ccb=7-4&oh=9e6337292135fed1bac807675d86862d&oe=61B9DBF7&_nc_sid=4f375e',
-  },
-  {
-    name: 'Sherin Akbar',
-    location: 'Guruvayoor',
-    achivements: 'Web Developer',
-    instagram: 'https://www.instagram.com/__sherin__akbar__/',
+    instagram: 'https://www.instagram.com/_reshu________/',
     photo: '',
   },
   {
-    name: 'Vrindha',
-    location: 'Guruvayoor',
-    achivements: 'Web Developer',
+    name: 'Reshma Mukandhan (VC)',
+    dob: '20 August 2001',
+    location: 'Puthur',
+    achivements: 'Folk Dance',
+    instagram: 'https://www.instagram.com/_reshu________/',
+    photo: '',
+  },
+  {
+    name: 'Sreeram',
+    dob: '03 September 2001',
+    location: 'Chiyyaram',
+    achivements: 'School Head Boy',
+    instagram: 'https://www.instagram.com/__sherin__akbar__/',
+    photo: './assets/sreeram.jpeg',
+  },
+  {
+    name: 'Vrinda V',
+    dob: '01 December 2000',
+    location: 'Kunnathangadi',
+    achivements: 'Athlete',
     instagram: 'https://www.instagram.com/vrindha.__/',
     photo: './assets/Vrinda.jpeg',
   },
   {
-    // name: 'ʀᴇꜱʜᴍᴀ_ᴍᴜᴋᴜɴᴅʜᴀɴ',
-    name: 'Reshma Mukandhan',
+    name: 'Vishal K S',
+    dob: '20 March 2002',
+    location: 'Mundoor',
+    achivements: 'Arts Captain',
+    instagram: 'https://www.instagram.com/_reshu________/',
+    photo: '',
+  },
+  {
+    name: 'Akhil K',
+    dob: '',
     location: 'Guruvayoor',
     achivements: 'Web Developer',
     instagram: 'https://www.instagram.com/_reshu________/',
     photo: '',
   },
+  {
+    name: 'Sherin Akbar',
+    dob: '28 January 2002',
+    location: 'Guruvayoor',
+    achivements: 'BasketBall',
+    instagram: 'https://www.instagram.com/__sherin__akbar__/',
+    photo: './assets/sherin.jpg',
+  },
+  {
+    name: 'Somkumar V A',
+    dob: '30 MAY 2001',
+    location: 'Guruvayoor',
+    achivements: 'Kabaddi',
+    instagram: 'https://www.instagram.com/somkumar_va/',
+    photo: './assets/somkumar.jpeg',
+  },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
   let displayMenu = ar.map((item) => {
+    //   return `<div class="card">
+    //   <div class="card-header" >
+    //     <img src="${item.photo}" class="card-profile-photo" alt="DP" />
+    //   </div>
+
+    //   <div>
+    //     <h2 class="card-profile-name">${item.name}</h2>
+    //     <div class="card-div">
+    //       <i class="fas fa-map-marker-alt"></i>
+    //       <h3>${item.location}</h3>
+    //     </div>
+    //     <div class="card-div">
+    //       <i class="fas fa-calendar-day"></i>v
+    //       <h3>${item.dob}</h3>
+    //     </div>
+    //     <div class="card-div">
+    //       <i class="fas fa-award"></i>
+    //       <h3>${item.achivements}</h3>
+    //     </div>
+    //   </div>
+    //   <a href="${item.instagram}" target="_blank" class="card-links" >
+    //     <i class="fab fa-instagram"></i>
+    //   </a>
+    // </div>`;
     return `<div class="card">
-    <img src="${item.photo}" class="card-profile-photo" alt="DP" />
-    <h2 class="card-profile-name">${item.name}</h2>
-    <div class="card-location">
-      <img src="./assets/location.svg" class="card-logos" alt="" />
-      <h3>${item.location}</h3>
+    <div class="card-header">
+        <img src="${item.photo}" alt="" />
+      </div>
+      <div class="card-body">
+        <h2>${item.name}</h2>
+        <div class="card-item">
+          <i class="fas fa-map-marker-alt"></i>
+          <h3>${item.location}</h3>
+        </div>
+        <div class="card-item">
+          <i class="fas fa-calendar-day"></i>
+          <h3>${item.dob}</h3>
+        </div>
+        <div class="card-item">
+          <i class="fas fa-award"></i>
+          <h3>${item.achivements}</h3>
+        </div>
     </div>
-    <div class="card-achivement">
-      <img src="./assets/award.svg" class="card-logos" alt="" />
-      <h3>${item.achivements}</h3>
+    <div class="card-links">
+      <a href="${item.instagram}" target="_blank" >
+        <i class="fab fa-instagram"></i>
+      </a>
     </div>
-    <a href="${item.instagram}" target="_blank">
-      <img src="./assets/instagram.svg" class="card-links" alt="" />
-    </a>
   </div>`;
   });
 
